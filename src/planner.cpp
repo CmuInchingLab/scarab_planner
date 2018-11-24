3a3b25de0fa0fe0220562ae44306c2c1348ac118
andrewww2 <awong2@andrew.cmu.edu>
2018-11-24 02:25:05 -0500

    wait for other nodes to init before sending messages

--- src/planner.cpp
+++ src/planner.cpp
@@ -18,10 +18,15 @@ struct Point{
 class Planner{
 private:
   ros::NodeHandle* n;
+
+  //handles the sending/receiving of messages to get Z value of map given an X and Y
   int counter = 0;
   ros::Publisher request_Z_pub;
   ros::Subscriber receive_Z_sub;
+
 public:
+  bool got_msg = true;     //true if we received the last Z value we requested
+
   Planner(ros::NodeHandle* nh){
     n = nh;
     request_Z_pub = n->advertise<scarab_gazebo::PointArr>("/scarab_gazebo/request_z", 10);
@@ -30,6 +35,10 @@ public:
 
   //get Z values from Gazebo node
   void requestZ(vector<Point> points){
+    while(!got_msg){  //wait until we've received previous message before sending this one  
+      // cout << "Waiting for message" << endl;
+      ros::spinOnce();
+    }
     scarab_gazebo::PointArr query_pts;
     query_pts.id = counter;
     for (const auto& p: points){
@@ -39,20 +48,27 @@ public:
       query_pts.points.push_back(query_p);
     }
     request_Z_pub.publish(query_pts);
-    counter++;
+    got_msg = false;
+    // cout << "Requested Z values" << endl;
+
   }
 
   // Callback for getting array of Z coordinates given X and Y coordinates
   void receiveZ(const scarab_gazebo::PointArr receivedPts){
       // print out coordinates for debugging
-      // if (receivedPts.id == counter-1){
+      if (receivedPts.id == counter){
         cout << "POINT(S) RECEIVED!" << endl; 
         for (const auto& rp: receivedPts.points){
           cout << "X:" << rp.x << "  Y:" << rp.y << "  Z:" << rp.z << endl; 
         }
-      // }
-      // else
-      //   ROS_ERROR("ID errror in planner.cpp receiveZ( ): query points and received points don't match!");
+        counter++;
+        got_msg = true;
+      }
+      else{
+        // cout << "EXPECTED ID:" << counter << " BUT GOT:" << receivedPts.id << endl;
+        ROS_ERROR("ID errror in planner.cpp receiveZ( ): query points and received points don't match!");
+      }
+
         
   }
 };
@@ -68,8 +84,11 @@ int main(int argc, char **argv)
   Point p3(-45.61109,-32.34);
   vector<Point> pts = {p1,p2, p3};
 
+  //wait for Gazebo node to startup. Otherwise messages will never be received
+  ros::Duration(1.5).sleep(); 
   while (ros::ok())
   {
+    // if (plan.got_msg)
     plan.requestZ(pts);
     ros::spinOnce();
     loop_rate.sleep();
