f31d94940b1cfabe83dd740edda87f9e08558308
Ritwik Das <ritwikdas54@gmail.com>
2018-11-30 15:47:13 -0500

    Generic Planner Works with Test

--- src/noros_planner.cpp
+++ src/noros_planner.cpp
@@ -22,12 +22,37 @@ struct State
 	double y;
 	double theta;
 	State(double a,double b,double c):x(a), y(b), theta(c){}
+	
+	friend ostream& operator<<(ostream& os, const State* s)
+	{
+		os << s->toString() << " ";
+		return os;
+	}
+	
+	string toString() const
+	{
+		return (to_string(this->x) + " " + to_string(this->y) + " " + to_string(this->theta));
+	}
+	
+	bool operator==(const State* rhs) const
+	{ 
+		double x_tol = 1; double y_tol = 1; double theta_tol = 1e-3;
+		return ((abs(this->x - rhs->x) < x_tol) && 
+				(abs(this->y - rhs->y) < y_tol) && 
+				(abs(this->theta - rhs->theta) < theta_tol));		
+	}
+
 };
 
 struct Action
 {
 	char dir;
 	Action(char d):dir(d){}
+	friend ostream& operator<<(ostream& os, const Action* a)
+	{
+		os << a->dir << " ";
+		return os;
+	}
 };
 
 struct Info
@@ -56,10 +81,7 @@ struct StateComparator
 {
 	bool operator()(const State* lhs, const State* rhs) const
 	{
-		double x_tol = 1; double y_tol = 1; double theta_tol = 1e-3;
-		return ((abs(lhs->x - rhs->x) < x_tol) && 
-				(abs(lhs->y - rhs->y) < y_tol) && 
-				(abs(lhs->theta - rhs->theta) < theta_tol));
+		return (*lhs == rhs);
 	}
 };
 
@@ -82,144 +104,110 @@ double a_star_search::get_heuristic(State* current,State* goal)
 	return 0;
 }
 bool a_star_search::get_successors(State* current, vector<tuple<State*,Action*,Info*>>& successors)
-{
-	State* a = new State(0,0,0);
-	State* b = new State(0,1,0);
-	State* c = new State(1,0,0);
-	State* d = new State(1,1,0);
-	
-	Info* i = new Info();
+{	
+	double M = 5;
+	Info* I = new Info();
 
-	if(current == a)
-	{
-		Action* a1 = new Action('E');
-		State* s1 = new State(0,1,0);
-		successors.push_back(make_tuple(s1,a1,i));
+	vector<vector<double>> dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+	vector<char> actions = {'N','E','S','W'};
+	for(int i=0;i<4;++i){
+		Action* A = new Action(actions[i]);
+		// cout<<dirs[i][0];
+		// cout<<min(max(current->x+dirs[i][0],0.0),M-1);
+		// cout<<min(max(current->y+dirs[i][1],0.0),M-1);
+		State* S = new State(min(max(current->x+dirs[i][0],0.0),M-1),min(max(current->y+dirs[i][1],0.0),M-1),0.0);
 
-		Action* a2 = new Action('S');
-		State* s2 = new State(1,0,0);
-		successors.push_back(make_tuple(s2,a2,i));
-
-		return true;
-
+		if(*current == S)
+		{
+			continue;
+		}
+		else
+		{
+			successors.push_back(make_tuple(S,A,I));
+		}
 	}
-	else if(current == b)
-	{
-		Action* a1 = new Action('W');
-		State* s1 = new State(0,0,0);
-		successors.push_back(make_tuple(s1,a1,i));
-
-		Action* a2 = new Action('S');
-		State* s2 = new State(1,1,0);
-		successors.push_back(make_tuple(s2,a2,i));
-
-		return true;
-
-	}
-	else if(current == c)
-	{
-		Action* a1 = new Action('E');
-		State* s1 = new State(1,1,0);
-		successors.push_back(make_tuple(s1,a1,i));
-
-		Action* a2 = new Action('N');
-		State* s2 = new State(0,0,0);
-		successors.push_back(make_tuple(s2,a2,i));
-		
-		return true;
-	} 
-	else if(current == d)
-	{
-		Action* a1 = new Action('W');
-		State* s1 = new State(0,1,0);
-		successors.push_back(make_tuple(s1,a1,i));
-
-		Action* a2 = new Action('N');
-		State* s2 = new State(1,0,0);
-		successors.push_back(make_tuple(s2,a2,i));
-
-		return true;
-	}
-
+	if(successors.size()){return true;}
 	return false;
 }
 
 bool a_star_search::get_plan(State* start,State* goal, vector<tuple<State*,Action*,Info*>>& path)
 {
-	// unordered_map<State*,double,StateHasher, StateComparator> cost_so_far;
-	// priority_queue<pair<double, State*>> f_state;
-	// f_state.push({get_heuristic(start,goal), start});
-	// came_from[start] = make_tuple(nullptr,nullptr,nullptr);
-	// cost_so_far[start] = 0;
-	// unordered_set<State*,StateHasher,StateComparator> visited;
+	unordered_map<State*,double,StateHasher,StateComparator> cost_so_far;
+	priority_queue<pair<double, State*>> f_state;
+	f_state.push({get_heuristic(start,goal), start});
+	came_from[start] = make_tuple(nullptr,nullptr,nullptr);
+	cost_so_far[start] = 0;
+	unordered_set<State*,StateHasher,StateComparator> visited;
 
-	// while (!f_state.empty())
-	// {
-	// 	pair<double,State*> node = f_state.top(); f_state.pop();
-	// 	State* current = node.second;
+	while (!f_state.empty())
+	{
+		pair<double,State*> node = f_state.top(); f_state.pop();
+		State* current = node.second;
 
-	// 	if (visited.find(current)!=visited.end())
-	// 	{
-	// 		continue;
-	// 	}
-	// 	if (current == goal) 
-	// 		{
+		if (visited.find(current)!=visited.end())
+		{
+			continue;
+		}
+		// cout<<"Insert State into Visited = "<<current<<"\n";
+		// cout<<"Current Priority Queue Size = "<<f_state.size()<<"\n";
+		visited.insert(current);
+		if (*current == goal) 
+			{
+			while(get<0>(came_from[current])!=nullptr)
+			{
+				tuple<State*,Action*,Info*> step_tuple = came_from[current];
+				path.push_back(step_tuple);
+				current = get<0>(step_tuple);
+			}
+			reverse(path.begin(),path.end());
+			cout<<"\n";
+			cout<<"GOAL REACHED!!!"<<"\n";
+			cout<<"Number of Expanded States = "<<visited.size()<<"\n";
+			return true;
+		}
+		vector<tuple<State*,Action*,Info*>> successors;
+		if(get_successors(current,successors)){
+			for (auto next : successors) 
+			{
+				State* next_state = get<0>(next);
+				Action* next_action = get<1>(next);
+				Info* next_info = get<2>(next);
 
-	// 		while(came_from[current]!=nullptr)
-	// 		{
-	// 			tuple<State*,Action*,Info*> step_tuple = came_from[current];
-	// 			path.push_back(step_tuple);
-	// 			current = get<0>(step_tuple);
-	// 		}
-			
-	// 		tuple<State*,Action*,Info*> step_tuple = came_from[current];
-	// 		path.push_back(step_tuple);
-
-	// 		reverse(path.begin(),path.end());
-	// 		cout<<"\n";
-	// 		cout<<"GOAL REACHED!!!"<<"\n";
-	// 		cout<<"Number of Expanded States = "<<visited.size();
-	// 		return true;
-	// 	}
-	// 	vector<tuple<State*,Action*,Info*>> successors;
-	// 	if(get_successors(current,successors)){
-	// 		for (auto next : successors) 
-	// 		{
-	// 			State* next_state = get<0>(next);
-	// 			Action* next_action = get<1>(next);
-	// 			Info* next_info = get<2>(next);
-
-	// 			if(visited.find(next_state)!=visited.end())
-	// 			{
-	// 				continue;
-	// 			}
-	// 			if(cost_so_far.find(current) == cost_so_far.end())
-	// 			{
-	// 				cost_so_far[current] = MAX_COST;
-	// 			}
-	// 			double new_cost = cost_so_far[current] + get_cost(current, next_state);
-	// 			if (cost_so_far.find(next_state) == cost_so_far.end() || new_cost < cost_so_far[next_state]) 
-	// 			{
-	// 				cost_so_far[next_state] = new_cost;
-	// 				double f_cost = new_cost + get_heuristic(next_state, goal);
-	// 				f_state.push({f_cost,next_state});
-	// 				came_from[next] = make_tuple(current,next_action,next_info);
-	// 			}
-	// 		}
-	// 	}
-	// 	visited.insert(current);
-	// }
-	// cout<<"Planning Failed; Please Try Again in a Few Moments";
+				if(visited.find(next_state)!=visited.end())
+				{
+					continue;
+				}
+				if(cost_so_far.find(current) == cost_so_far.end())
+				{
+					cost_so_far[current] = MAX_COST;
+				}
+				double new_cost = cost_so_far[current] + get_cost(current, next_state);
+				if (cost_so_far.find(next_state) == cost_so_far.end() || new_cost < cost_so_far[next_state]) 
+				{
+					cost_so_far[next_state] = new_cost;
+					double f_cost = new_cost + get_heuristic(next_state, goal);
+					f_state.push({f_cost,next_state});
+					came_from[next_state] = make_tuple(current,next_action,next_info);
+				}
+			}
+		}
+	}
+	cout<<"Planning Failed; Please Try Again in a Few Moments"<<"\n";
 	return false;
 }
 
 int main(){
 
 	State* start = new State(0,0,0);
-	State* one = new State(0,1,0);
-	State* two = new State(1,0,0);
-	State* goal = new State(1,1,0);		
+	State* goal = new State(4,4,0);		
+
 	a_star_search* planner = new a_star_search();
 	vector<tuple<State*,Action*,Info*>> path;
 	planner->get_plan(start,goal,path);
+	cout<<"Printing Plan"<<"\n";
+	for(auto element:path)
+	{
+		cout<<get<0>(element)<<get<1>(element)<<"\n";
+	}
+	
 }
